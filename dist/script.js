6436ef411ebb7c8aad065131ce0127b81949d49b
rayneskarlcydrick@gmail.com <rayneskarlcydrick@gmail.com>
2023-03-22 20:53:30 +0800

    transform

--- dist/script.js
+++ dist/script.js
@@ -1,13 +1,19 @@
 const toggle__burger = document.querySelector(".toggle__burger");
 const navigation = document.querySelector(".navigation");
+const toggle__dot = document.querySelector(".toggle__dot");
+const time = document.querySelector(".time");
 toggle__burger.addEventListener("click", () => {
 toggle__burger.classList.toggle("open");
 navigation.classList.toggle("open");
+time.classList.remove("open");
+toggle__dot.classList.remove("open");
 });
 
-const toggle__dot = document.querySelector(".toggle__dot");
-const time = document.querySelector(".time");
+
 toggle__dot.addEventListener("click", () => {
 toggle__dot.classList.toggle("open");
 time.classList.toggle("open");
+navigation.classList.remove("open");
+toggle__burger.classList.remove("open");
+
 })
