418fb96e9070430f529331e882078e0ac1a459cc
kcryns <rayneskarlcydrick@gmail.com>
2023-03-23 15:51:42 +0800

    latest

--- dist/script.js
+++ dist/script.js
@@ -13,4 +13,24 @@ toggle__dot.classList.remove("open");
 toggle__dot.addEventListener("click", () => {
 toggle__dot.classList.toggle("open");
 time.classList.toggle("open");
+navigation.classList.remove("open");
+toggle__burger.classList.remove("open");
+});
+
+
+const dropp = document.querySelectorAll(".fa-chevron-down");
+dropp.forEach((dropps) => {
+    dropps.addEventListener('click',()=>{
+        const el = dropps.parentElement.parentElement;
+        el.classList.toggle("open");
+        dropps.classList.toggle("open");
+        dropps.parentElement.parentElement.classList.toggle("active");
+    })
 })
+// const drop_down = document.querySelectorAll(".navigation .drop_down")
+// drop_down.forEach((dropd) => {
+//     dropd.addEventListener("click", () => {
+//         dropd.nextElementSibling.classList.toggle("open");
+//         dropd.querySelector("i").classList.toggle("open");
+//     });
+// });
