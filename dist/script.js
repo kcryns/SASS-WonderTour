9077f7a37bcbd2b11ec57f2f7d6e3b060e5bfd17
kcryns <rayneskarlcydrick@gmail.com>
2023-03-24 15:55:04 +0800

    nice

--- dist/script.js
+++ dist/script.js
@@ -18,19 +18,63 @@ toggle__burger.classList.remove("open");
 });
 
 
-const dropp = document.querySelectorAll(".fa-chevron-down");
-dropp.forEach((dropps) => {
-    dropps.addEventListener('click',()=>{
-        const el = dropps.parentElement.parentElement;
-        el.classList.toggle("open");
-        dropps.classList.toggle("open");
-        dropps.parentElement.parentElement.classList.toggle("active");
-    })
-})
-// const drop_down = document.querySelectorAll(".navigation .drop_down")
-// drop_down.forEach((dropd) => {
-//     dropd.addEventListener("click", () => {
-//         dropd.nextElementSibling.classList.toggle("open");
-//         dropd.querySelector("i").classList.toggle("open");
-//     });
-// });
+// const dropp = document.querySelectorAll(".fa-chevron-down");
+// dropp.forEach((dropps) => {
+//     dropps.addEventListener('click',()=>{
+//         const el = dropps.parentElement.parentElement;
+//         el.classList.toggle("open");
+//         dropps.classList.toggle("open");
+//         dropps.parentElement.parentElement.classList.toggle("active");
+//     })
+// })
+const drop_down = document.querySelectorAll(".drop_down .li__head")
+drop_down.forEach((dropd) => {
+    dropd.addEventListener("click", () => {
+     const content = dropd.nextElementSibling;
+     const icon = dropd.querySelector("i");
+     const head = dropd.parentElement;
+     if (head.classList.contains("active")){
+        content.classList.remove("open");
+        icon.classList.remove("open");
+        head.classList.remove("active");
+
+     } else{
+        yow();
+        content.classList.toggle("open");
+        icon.classList.toggle("open");
+        head.classList.toggle("active");
+     }
+    });
+});
+
+const yow = () => {
+    drop_down.forEach((dropd) => {
+        // dropd.addEventListener("click", () => {
+            // console.log(dropd.nextElementSibling)
+            // dropd.nextElementSibling.classList.remove("open");
+            // dropd.querySelector("i").classList.remove("open");
+            // dropd.parentElement.classList.remove("active");
+        // });
+        
+        // console.log(dropd)
+        // const head = dropd.nextElementSibling;
+        // console.log(head)
+       
+    });
+    // console.log(drop_down);
+}
+
+
+
+var slider = tns({
+        container: '.banner__banner',
+        items: 1,
+        slideBy: 'page',
+        autoplay: true,
+        axis: "vertical",
+        controls: false,
+        nav: false,
+        autoplayPosition: false,
+        autoplayButton:false,
+    });
+    
